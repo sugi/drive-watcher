a49300dd9ad9f39f2a36e948c9e6fae4ba07aaa8
Tatsuki Sugiura <sugi@nemui.org>
2012-10-27 04:28:21 +0900

    Init app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DriveWatcher::Application.config.secret_token = '7886cc4c6113b2ab5564b4967ee473e9236ec09a8ad323291f21caf9a34dacc19d3c5b4005b9c4c24a5e2fbde16596deea4f8e9a55bd80227244547a2347424f'
