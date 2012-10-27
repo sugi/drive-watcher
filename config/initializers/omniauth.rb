69e2cc2037c0428828f80a5b81f337ae6dbda517
Tatsuki Sugiura <sugi@nemui.org>
2012-10-28 07:52:12 +0900

    Add omniauth initalizer.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,18 @@
+Rails.configuration.google_client_configured = false
+
+begin
+  gsetting = YAML.load_file "#{Rails.root}/config/google-api.yml"
+  gsetting["client_id"].blank?  and raise Errno::ENOENT
+  gsetting["api_secret"].blank? and raise Errno::ENOENT
+
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :google_oauth2, gsetting["client_id"], gsetting["client_secret"], {
+      access_type: 'offline',
+      scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/drive.readonly',
+      # redirect_uri:'http://localhost:3000/auth/google_oauth2/callback'
+    }
+    Rails.configuration.google_client_configured = true
+  end
+rescue Errno::ENOENT
+  # ignore
+end
