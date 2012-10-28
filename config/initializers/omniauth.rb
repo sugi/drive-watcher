85bd4fbeb124d96c709308fe0e08fef2eba60758
Tatsuki Sugiura <sugi@nemui.org>
2012-10-28 16:57:03 +0900

    Fix typo on omniauth initalizer.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.configuration.google_client_configured = false
 begin
   gsetting = YAML.load_file "#{Rails.root}/config/google-api.yml"
   gsetting["client_id"].blank?  and raise Errno::ENOENT
-  gsetting["api_secret"].blank? and raise Errno::ENOENT
+  gsetting["client_secret"].blank? and raise Errno::ENOENT
 
   Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, gsetting["client_id"], gsetting["client_secret"], {
@@ -16,3 +16,6 @@ begin
 rescue Errno::ENOENT
   # ignore
 end
+
+Rails.configuration.google_client_configured or
+  $stderr.puts "Google API client is not configured!\nSet your client information at #{Rails.root}/config/google-api.yml."
