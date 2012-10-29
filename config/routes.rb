fc6d3b5c7f345166ebf48bf53c5b93c2c7cc4112
Tatsuki Sugiura <sugi@nemui.org>
2012-10-29 23:57:29 +0900

    Fix session controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 DriveWatcher::Application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :sessions => 'users/sessions' }
   devise_scope :user do
-    get 'sign_in',  :to => 'devise/sessions#new',     :as => :new_user_session  # TODO: fix
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    get 'sign_in',  :to => 'users/sessions#new',     :as => :new_user_session  # TODO: fix
+    get 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
   end
 
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
