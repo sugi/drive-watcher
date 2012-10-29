7b2c960950a768054d34e05d6c3280bb739f8911
Tatsuki Sugiura <sugi@nemui.org>
2012-10-30 08:12:59 +0900

    Use flash responder.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  class MyResponder < ActionController::Responder
+    include Responders::FlashResponder
+  end
   protect_from_forgery
   respond_to :html, :xml, :json
+  self.responder = MyResponder
 end
